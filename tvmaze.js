6fc586fbd6c3bebfd968730d5f3dbc5c0af81b06
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2022-11-17 08:39:37 -0800

    implemented fetching of episodes

--- tvmaze.js
+++ tvmaze.js
@@ -5,6 +5,7 @@ const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 const TVMAZE_BASE_URL = 'http://api.tvmaze.com';
 const TVMAZE_SHOW_ENDPOINT = '/search/shows';
+// const TVMAZE_EPISODES_ENDPOINT = `shows/SHOW-ID-HERE/episodes`
 const BLANK_IMAGE = "https://www.nbmchealth.com/wp-content/uploads/2018/04/default-placeholder.png"
 
 
@@ -93,9 +94,18 @@ async function searchForShowAndDisplay() {
 }
 
 /** listens to search form being submitted then calls controller function */
+
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
+});
+
+$('#showsList').on("click", "button", function (evt) {
+  const $curBtn = $(evt.target);
+  // console.log($curBtn);
+  const showId = $curBtn.parent().parent().parent().data("showId");
+  getEpisodesOfShow(showId);
+  // console.log(showId);
 });
 
 
@@ -103,11 +113,41 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+  const searchUrl = `${TVMAZE_BASE_URL}/shows/${id}/episodes`;
+  const resp = await axios.get(searchUrl);
+  console.log(resp);
+  const episodes = serializeEpisodes(resp.data);
+  populateEpisodes(episodes);
+ }
+
+
+ function serializeEpisodes(episodes){
+  const results = [];
+
+  for(const episode of episodes){
+    const {id, name, season, number} = episode;
+    results.push({id, name, season, number});
+  }
+
+  console.log("results=", results);
+  return results;
+
+ }
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) {
+  $episodesArea.show();
+
+  for(const {id, name, season, number} of episodes){
+    $episodesArea.append(
+      $(`
+        <p>${name} (season ${season}, number ${number})</p>
+      `)
+    )
+  }
+}
 
 //    * http://api.tvmaze.com/shows/[showid]/episodes
 
